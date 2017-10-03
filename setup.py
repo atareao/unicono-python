5c473509178dfcbf99e3525f265ec0f1953a7639
atareao <lorenzo.carbonell.cerezo@gmail.com>
2017-10-03 12:53:14 +0200

    First commit of python version

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+from distutils.core import setup
+
+setup(name='unicono-python',
+      version='0.0.1',
+      author='Lorenzo Carbonell',
+      author_email='lorenzo.carbonell.cerezo@gmail.com',
+      url='https://www.atareao.es',
+      license='GPLv3',
+      data_files=[('/usr/share/applications',
+                  ['data/extras-pushbullet-indicator.desktop'])])
